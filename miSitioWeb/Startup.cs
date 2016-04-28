8087c35953077b6d5a775401bae27dc69156c871
Armando Nevares <armandonl14@gmail.com>
2016-04-27 19:50:11 -0600

    proyecto

---
+++ miSitioWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(miSitioWeb.Startup))]
+namespace miSitioWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
